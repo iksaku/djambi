0e953ad4a1eb71088732ef9828208cf76c249bec
Jorge González <yo@jorgeglz.io>
2021-02-13 17:10:30 -0600

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+      './index.html',
+      './src/**/*.{vue,js,ts}'
+  ],
+
+  darkMode: false, // or 'media' or 'class'
+
+  theme: {
+    extend: {},
+  },
+
+  variants: {
+    extend: {},
+  },
+
+  plugins: [],
+}
